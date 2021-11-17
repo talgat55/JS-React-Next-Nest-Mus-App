1ab4e3283667d9af8c1c45c562873cfef5180603
talgat55 <lightx_design@yahoo.com>
2021-11-18 02:09:39 +0600

    add part track and album

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+        MongooseModule.forRoot('mongodb://localhost/nest'),
+        TrackModule,
+        FileModule
+    ]
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from "./app.module";
+const start = async () =>{
+    try {
+        const PORT = process.env.PORT || 5000;
+        const app = await NestFactory.create(AppModule);
+        await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
+    } catch (e){
+        console.log(e)
+    }
 }
-bootstrap();
+start()
